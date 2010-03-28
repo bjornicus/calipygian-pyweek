d33e58b66ae15026a03e4471d20e40d2b1689996
sharru.kin <sharru.kin@4f94c154-1fe4-7ef1-e36f-7715520f0143>
2010-03-28 08:53:47 +0000

    Here is the prototype for Ship movement I promised.
    We now have a very Dadaistic ship bobbing up and down. At 5 seconds we change the amplitude of the waveform describing the ships movement, and at 10 we change the frequency. These operations are SMOOTH! meaning the ship does not suddenly jump position (I hope). 
    
    Next up is to wire in user controls so we an move this ship around ourselves.
    
    Also, if anyone wants to be awesome, adding sprite support would make life easier for when I/someone else wants to play with the course "Preview" option, where we project the ships course across the screen dynamically.
    
    -G

--- trunk/pyweek10/gamelib/levels.py
+++ trunk/pyweek10/gamelib/levels.py
@@ -2,6 +2,25 @@
 @author: bjorn
 '''
 import pyglet
+import gamestate
+import math
+
+
+##FIXME Temporary testing hacks Remove these!
+playership = gamestate.ship()
+
+def TestAdjustAmp(dt):
+    print("Adjusting Ship Amplitude")
+    playership.AdjustAmplitude(.4)
+
+def TestAdjustFreq(dt):
+    print("Adjusting Ship Frequency")
+    playership.AdjustAngularFrequency(.7 * math.pi)
+
+def TestAdjustPhase(dt):
+    print("Adjusting Ship Phase")
+    playership.AdjustPhase( math.pi )
+##End of FIXME
 
 class LevelBase(object):
     '''
@@ -30,3 +49,25 @@ class LevelOne(LevelBase):
         LevelBase.__init__(self, window)
         label = pyglet.text.Label('LEVEL 1!')
         self.renderlist.append(label)
+        
+        ## More Test Hacks
+        self.shiplabel = pyglet.text.Label('Ship', x=0, y=self.window.height//2)
+        
+        pyglet.clock.schedule_interval(self.update, 1/60.0)
+        pyglet.clock.schedule_once(TestAdjustAmp, 5.0)
+        pyglet.clock.schedule_once(TestAdjustFreq, 10.0)
+        pyglet.clock.schedule_once(TestAdjustPhase, 15.0)
+        
+    def update(self, dt):
+        ##Temporary hack, we need to come up with a better way of managing gamestate
+        playership.Update(dt)
+        
+        
+    def on_draw(self):
+        LevelBase.on_draw(self)
+        
+        ##Yet More Hacking
+        shipPos = playership.GetPosition()
+        shipPosInWindow = self.window.height//2 + (shipPos * self.window.height//2)
+        self.shiplabel.y=shipPosInWindow
+        self.shiplabel.draw()
