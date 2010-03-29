698ce0bdb950a97b0c665eea057c972e11e7ee90
jlnordin <jlnordin@4f94c154-1fe4-7ef1-e36f-7715520f0143>
2010-03-29 07:44:55 +0000

    Ship visualization! I also decreased the number of steps in the oscillation viewer to speed things up a bit. Visuals for the oscillator is forth coming.
    I refactored a bit of the level class with this, so I think some of the tests might break. I have yet to get Nose test installed and working, and it's late, so I'll have to revisit the testing later.

--- trunk/pyweek10/gamelib/levels.py
+++ trunk/pyweek10/gamelib/levels.py
@@ -1,5 +1,5 @@
 '''
-Contains code for different levels of the game. 
+Contains code for different levels of the game.
 Levels render in a window, handle input, and update
 the entities they contain.
 
@@ -10,11 +10,12 @@ from pyglet.window import key
 
 import gamestate
 import random
+import player
 
 SECONDS_TO_CROSS_SCREEN = 2
 
 ##FIXME Temporary testing hacks Remove these!
-playership = gamestate.Oscillator()
+playership = player.Player()
 shiplabel = pyglet.text.Label('Ship', x=0, y=240)
 predictive_label = pyglet.text.Label('-')
 rock_label = pyglet.text.Label('ROCK!')
@@ -35,10 +36,14 @@ class LevelBase(object):
         self.renderlist = []
         self.actorlist = []
         self.reactorlist = [] # list of objects expecting to pool keyboard state when they update
-        
-        self.renderlist.append(shiplabel)
+        self.fps_display = pyglet.clock.ClockDisplay()
+
+        playership.SetWindowHeight(self.window.height)
+
+        self.renderlist.append(playership)
+        self.renderlist.append(self.fps_display)
         self.reactorlist.append(playership)
-        
+
         self.window.push_handlers(self.keystate)
         pyglet.clock.schedule_interval(self.update, 1/60.0)
 
@@ -46,13 +51,13 @@ class LevelBase(object):
         self.window.clear()
         for drawable in self.renderlist:
             drawable.draw()
-        
+
     def update(self, dt):
         for actor in self.actorlist:
             actor.Tick(dt)
         for reactor in self.reactorlist:
             reactor.Tick(dt, self.keystate)
-            
+
         ## Hacks
         for rock in rocks:
             rock["x"] = rock["x"] - ((dt/SECONDS_TO_CROSS_SCREEN)* self.window.width)
@@ -60,7 +65,7 @@ class LevelBase(object):
                 rocks.remove(rock)
         rockprob = random.randrange(100)
         if (rockprob > 95) and (len(rocks) < 5):
-            rocks.append({"x":self.window.width, "y":random.randrange(self.window.height)})        
+            rocks.append({"x":self.window.width, "y":random.randrange(self.window.height)})
 
 class LevelOne(LevelBase):
     '''
@@ -68,24 +73,17 @@ class LevelOne(LevelBase):
     '''
     def __init__(self, window):
         LevelBase.__init__(self, window)
-        
+
     def on_draw(self):
         LevelBase.on_draw(self)
-        
-        ##Yet More Hacking
-        shipPos = playership.GetPosition()
-        shipPosInWindow = self.window.height//2 + (shipPos * self.window.height//2)
-        shiplabel.y=shipPosInWindow
-        
-        
-        for (t, y) in playership.GetPredictivePath(.1, SECONDS_TO_CROSS_SCREEN, .01):
+
+        for (t, y) in playership.GetPredictivePath(.1, SECONDS_TO_CROSS_SCREEN, .1):
             offset = self.window.width * (t/SECONDS_TO_CROSS_SCREEN)
             predictive_label.y = self.window.height//2 + (y * self.window.height//2)
             predictive_label.x = offset
             predictive_label.draw()
-            
+
         for rock in rocks:
             rock_label.x = rock["x"]
             rock_label.y = rock["y"]
             rock_label.draw()
-            
