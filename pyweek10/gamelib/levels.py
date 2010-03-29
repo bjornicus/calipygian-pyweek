5b425e40eaa2c24daea9fc0add57af64ff39d494
sharru.kin <sharru.kin@4f94c154-1fe4-7ef1-e36f-7715520f0143>
2010-03-29 01:43:15 +0000

    LOOK OUT, ROCKS!

--- pyweek10/gamelib/levels.py
+++ pyweek10/gamelib/levels.py
@@ -5,11 +5,16 @@ import pyglet
 from pyglet.window import key
 
 import gamestate
+import random
+
+SECONDS_TO_CROSS_SCREEN = 2
 
 ##FIXME Temporary testing hacks Remove these!
 playership = gamestate.Oscillator()
 shiplabel = pyglet.text.Label('Ship', x=0, y=240)
 predictive_label = pyglet.text.Label('-')
+rock_label = pyglet.text.Label('ROCK!')
+rocks = []
 
 ##End of FIXME
 
@@ -43,6 +48,15 @@ class LevelBase(object):
             actor.Tick(dt)
         for reactor in self.reactorlist:
             reactor.Tick(dt, self.keystate)
+            
+        ## Hacks
+        for rock in rocks:
+            rock["x"] = rock["x"] - ((dt/SECONDS_TO_CROSS_SCREEN)* self.window.width)
+            if rock["x"] < 0:
+                rocks.remove(rock)
+        rockprob = random.randrange(100)
+        if (rockprob > 95) and (len(rocks) < 5):
+            rocks.append({"x":self.window.width, "y":random.randrange(self.window.height)})        
 
 class LevelOne(LevelBase):
     '''
@@ -59,9 +73,15 @@ class LevelOne(LevelBase):
         shipPosInWindow = self.window.height//2 + (shipPos * self.window.height//2)
         shiplabel.y=shipPosInWindow
         
-        for (t, y) in playership.GetPredictivePath(.1, 2.0, .01):
-            offset = self.window.width * (t/2.0)
+        
+        for (t, y) in playership.GetPredictivePath(.1, SECONDS_TO_CROSS_SCREEN, .01):
+            offset = self.window.width * (t/SECONDS_TO_CROSS_SCREEN)
             predictive_label.y = self.window.height//2 + (y * self.window.height//2)
             predictive_label.x = offset
             predictive_label.draw()
+            
+        for rock in rocks:
+            rock_label.x = rock["x"]
+            rock_label.y = rock["y"]
+            rock_label.draw()
             
