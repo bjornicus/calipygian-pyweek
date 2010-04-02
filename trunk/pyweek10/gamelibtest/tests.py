ccbe92df12a879b6fbc484f843c170e9d998e4d8
holomorph <holomorph@4f94c154-1fe4-7ef1-e36f-7715520f0143>
2010-04-02 06:35:17 +0000

    fix tests

--- trunk/pyweek10/gamelibtest/tests.py
+++ trunk/pyweek10/gamelibtest/tests.py
@@ -3,12 +3,13 @@
 '''
 from numpy.testing.utils import *
 from gamelib.levels import *
-from gamelib.gamestate import *
+from gamelib.entities import *
+from gamelib.constants import *
 import math
 
 from pyglet.window import key
 
-def test_LevelBase_on_draw_clears_window():
+def test_Levellevelbase_on_draw_clears_window():
     class mock_Window:
         def clear(self):
             self.clear_called = True
@@ -16,82 +17,60 @@ def test_LevelBase_on_draw_clears_window():
             pass
 
     mockWindow = mock_Window()
-    base = LevelBase()
-    base.window = mockWindow
-    base.on_draw()
+    levelbase = LevelBase()
+    levelbase.window = mockWindow
+    levelbase.on_draw()
 
     assert mockWindow.clear_called
  
-def test_LevelBase_on_draw_calls_draw_on_all_items_in_renderlist():
-    class mock_Window:
-        def clear(self):
-            pass
-        def push_handlers(self, handler):
-            pass
-
-    class mock_drawable:
-        def draw(self):
-            self.draw_called = True
-
-    drawable1 = mock_drawable()
-    drawable2 = mock_drawable()
-    base = LevelBase()
-    base.window = mock_Window()
-    base.renderlist.append(drawable1)
-    base.renderlist.append(drawable2)
-    base.on_draw()
-
-    assert drawable1.draw_called
-    assert drawable2.draw_called
-
 #------------- oscillator tests --------------#
 def test_Oscillator_AdjustFrequency_does_not_change_position():
     oscillator = Oscillator()
-    oscillator._Omega = gamestate.MIN_FREQUENCY
+    oscillator._Omega = MIN_FREQUENCY
       
     for t_step in range(0,200):
         
-        old_x = oscillator.GetPosition()
+        old_x = oscillator.GetCurrentValue()
         oscillator.AdjustAngularFrequency(0)
-        new_x = oscillator.GetPosition()
+        new_x = oscillator.GetCurrentValue()
         assert abs(old_x - new_x) < 0.01
         
         for f_step in [0.01, 0.05, 0.1, 0.5]:
-            while (oscillator._Omega + f_step < gamestate.MAX_FREQUENCY):
-                old_x = oscillator.GetPosition()
+            while (oscillator._Omega + f_step < MAX_FREQUENCY):
+                old_x = oscillator.GetCurrentValue()
                 oscillator.AdjustAngularFrequency(f_step)
-                new_x = oscillator.GetPosition()
+                new_x = oscillator.GetCurrentValue()
                 assert abs(old_x - new_x) < 0.01
                 
-            while (oscillator._Omega - f_step > gamestate.MIN_FREQUENCY):
-                old_x = oscillator.GetPosition()
+            while (oscillator._Omega - f_step > MIN_FREQUENCY):
+                old_x = oscillator.GetCurrentValue()
                 oscillator.AdjustAngularFrequency(-f_step)
-                new_x = oscillator.GetPosition()
+                new_x = oscillator.GetCurrentValue()
                 assert abs(old_x - new_x) < 0.01
         oscillator.Tick(0.01, pyglet.window.key.KeyStateHandler())
 
 def test_Oscillator_AdjustAmplitude_does_not_change_position():
     oscillator = Oscillator()
-    oscillator._Amplitude = gamestate.MIN_AMPLITUDE
+    oscillator._Amplitude = MIN_AMPLITUDE
           
     for t_step in range(0,200):
         
-        old_x = oscillator.GetPosition()
+        old_x = oscillator.GetCurrentValue()
         oscillator.AdjustAmplitude(0)
-        new_x = oscillator.GetPosition()
+        new_x = oscillator.GetCurrentValue()
         assert abs(old_x - new_x) < 0.01
         
         for a_step in [.01*math.pi, .05*math.pi, .1*math.pi, .5*math.pi]:
-            while (oscillator._Amplitude + a_step < gamestate.MAX_AMPLITUDE):
-                old_x = oscillator.GetPosition()
+            while (oscillator._Amplitude + a_step < MAX_AMPLITUDE):
+                old_x = oscillator.GetCurrentValue()
                 oscillator.AdjustAmplitude(a_step)
-                new_x = oscillator.GetPosition()
+                new_x = oscillator.GetCurrentValue()
                 assert abs(old_x - new_x) < 0.01
                 
-            while (oscillator._Amplitude - a_step > gamestate.MIN_AMPLITUDE):
-                old_x = oscillator.GetPosition()
+            while (oscillator._Amplitude - a_step > MIN_AMPLITUDE):
+                old_x = oscillator.GetCurrentValue()
                 oscillator.AdjustAmplitude(-a_step)
-                new_x = oscillator.GetPosition()
+                new_x = oscillator.GetCurrentValue()
                 assert abs(old_x - new_x) < 0.01
         oscillator.Tick(0.01, pyglet.window.key.KeyStateHandler())
         
