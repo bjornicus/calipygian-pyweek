994cb7fa6391308836677fb2c9adc8082957af36
holomorph <holomorph@4f94c154-1fe4-7ef1-e36f-7715520f0143>
2011-04-04 07:10:19 +0000

    dirt and grass tyles, easily editable map

--- pyweek12/ennead/platformer.py
+++ pyweek12/ennead/platformer.py
@@ -2,21 +2,27 @@ import pyglet
 from spaces import GameObject, CordinateSpace
 from constants import *
 
+TILE_SIZE_IN_PIXELS = 30
+LEVEL_WIDTH_IN_TILES = 27
+LEVEL_HEIGHT_IN_TILES = 9
 
-def setup_platformer(gamespace):
-    PlatformSpace = CordinateSpace()
-    PlatformSpace.width = WINDOW_WIDTH
-    PlatformSpace.height = WINDOW_HEIGHT - PUZZLE_BLOCK_SIDE_PIXEL_LENGTH
+def setup_platformer():
+    platforspace = CordinateSpace()
+    platforspace.width = LEVEL_WIDTH_IN_TILES * TILE_SIZE_IN_PIXELS
+    platforspace.height = LEVEL_HEIGHT_IN_TILES * TILE_SIZE_IN_PIXELS
     
-    for x in range(0, WINDOW_WIDTH, PUZZLE_BLOCK_SIDE_PIXEL_LENGTH):
-        PlatformSpace.AddObject(GrassBlock(), x, 0)
-    PlatformSpace.AddObject(PlayerBlock(), 0, PUZZLE_BLOCK_SIDE_PIXEL_LENGTH)
+    for x in range(0, LEVEL_WIDTH_IN_TILES):
+        for y in range(0, LEVEL_HEIGHT_IN_TILES):
+            tyle_type = level_map[y][x]
+            x_coord = x * TILE_SIZE_IN_PIXELS
+            y_coord = y * TILE_SIZE_IN_PIXELS
+            platforspace.AddObject(level_map_key[tyle_type](), x_coord, y_coord)
 
-    gamespace.AddObject(PlatformSpace, 0, PUZZLE_BLOCK_SIDE_PIXEL_LENGTH)
+    return platforspace
 
 
-class PlatformElement(GameObject):
-    GameObjectType = "PlatformElement"
+class Tile(GameObject):
+    GameObjectType = "Tile"
 
     def __init__(self, SpriteName):
         GameObject.__init__(self)
@@ -28,19 +34,44 @@ class PlatformElement(GameObject):
         x,y = xy_pos
         self.Sprite.blit(x,y)
 
-
-class GrassBlock(PlatformElement):
-    GameObjectType = "GrassBlock"
-
-    def __init__(self):
-        PlatformElement.__init__(self, 'Grass_Block.png')
-
-
-class PlayerBlock(PlatformElement):
-    GameObjectType = "PlayerBlock"
+class EmptyTile(GameObject):
+    GameObjectType = "Empty"
 
     def __init__(self):
-        PlatformElement.__init__(self, 'Player_Block.png')
+        GameObject.__init__(self)
+
+class Grass(Tile):
+    GameObjectType = "Grass"
+
+    def __init__(self):
+        Tile.__init__(self, 'grass.png')
+
+class Dirt(Tile):
+    GameObjectType = "Dirt"
+
+    def __init__(self):
+        Tile.__init__(self, 'dirt.png')
+
+level_map_key = { 0:EmptyTile, 1:Grass, 2:Dirt }
+
+level_map = [
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,2,2,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,2,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0,0,0],
+        [1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,1,1]
+        ]
+level_map.reverse()
+
+class Player(GameObject):
+    GameObjectType = "Player"
+
+    def __init__(self):
+        GameObject.__init__(self)
 
     def Update(self, delta_t):
         cord = self.GetCordinatesInParentSpace()
