e25c12ef43a98552d0fe0605b1bd360015724a4b
sharru.kin <sharru.kin@4f94c154-1fe4-7ef1-e36f-7715520f0143>
2011-04-05 07:12:19 +0000

    Checking in broken code :( Unfortunatly looks like I won't be fixing this tonight.
    I have most of a culling algorithm down. Unfortunately my "CheckForRectangleIntersection" is wrong. Fix that, and it all should work.... Unfortunately I don't think it will help our perf issues (it might make them worse over the massive overdraw)... I'll keep experimenting to find out.

--- pyweek12/ennead/spaces.py
+++ pyweek12/ennead/spaces.py
@@ -25,6 +25,12 @@ class SpaceCordinate():
     
     def set_y(self, y):
         self._y = y
+
+    def adjust_x(self, dx):
+        self._x += x
+    
+    def adjust_y(self, dy):
+        self._y += y
     
     def __repr__(self):
         return "SpaceCordinate(%s, %d, %d)" % (self.Space, self._x, self._y)
@@ -33,35 +39,121 @@ class SpaceCordinate():
         return "SpaceCordinate(%s, %d, %d)" % (self.Space, self._x, self._y)
 
     def AdaptCordinateToContainedObject(self, TargetObject):
+        space = self.Space
+        while not TargetObject in space:
+            space = space.parent_space
+
+        if space is None:
+            print("Warning: Atempted to adapt cordinate %s for object %s, "+
+                  "but they do not share a common space!" %
+                  (self, TargetObject))
+            return None
+        
         ObjectCord = self.Space.GetCordinatesOfContainedObject(TargetObject)
-        NegativeCord = SpaceCordinate(None, -ObjectCord.get_x(), -ObjectCord.get_y())
 
-        return RefCordinate(self, NegativeCord, TargetObject)
+        retval = RefCordinate(self, ObjectCord, TargetObject, negative=True)
+        return retval
 
+    def NewCordAtThisPosition(self):
+        return SpaceCordinate(self.space, self.get_x(), self.get_y())
+
+class OffsetCordinate(SpaceCordinate):
+    def __init__(self, baseCord, x, y):
+        SpaceCordinate.__init__(self, baseCord.Space)
+        self._baseCord = baseCord
+        self.xOffset = x
+        self.yOffset = y
+
+    def get_x(self):
+        return self._baseCord.get_x() + self.xOffset
+    
+    def get_y(self):
+        return self._baseCord.get_y() + self.yOffset
+
+    def set_x(self, x):
+        self._baseCord.set_x(x - self.xOffset)
+    
+    def set_y(self, y):
+        self._baseCord.set_y(y - self.yOffset)
+
+    def adjust_x(self, dx):
+        self._baseCord.adjust_x(dx)
+    
+    def adjust_y(self, dy):
+        self._baseCord.adjust_y(dy)
+    
+    def __repr__(self):
+        return "OffsetCordinate(%s, %d, %d)" % (self._baseCord, self.xOffset, self.yOffset)
+
+    def __str__(self):
+        return "OffsetCordinate(%s, %d, %d)" % (self._baseCord, self.xOffset, self.yOffset)
+
+        
 class RefCordinate(SpaceCordinate):
-    def __init__(self, baseCord, offsetCord, space):
+    def __init__(self, baseCord, offsetCord, space, negative=False):
         SpaceCordinate.__init__(self, space)
         self._baseCord = baseCord
         self._offsetCord = offsetCord
+        self._negative = negative
 
     def get_x(self):
-        return self._baseCord.get_x() + self._offsetCord.get_x()
+        if not self._negative:
+            return self._baseCord.get_x() + self._offsetCord.get_x()
+        else:
+            return self._baseCord.get_x() - self._offsetCord.get_x()
     
     def get_y(self):
-        return self._baseCord.get_y() + self._offsetCord.get_y()
+        if not self._negative:
+            return self._baseCord.get_y() + self._offsetCord.get_y()
+        else:
+            return self._baseCord.get_y() - self._offsetCord.get_y()
 
     def set_x(self, x):
-        self._baseCord.set_x(x - self._offsetCord.get_x())
+        if not self._negative:
+            self._baseCord.set_x(x - self._offsetCord.get_x())
+        else:
+            self._baseCord.set_x(x + self._offsetCord.get_x())
     
     def set_y(self, y):
-        self._baseCord.set_y(y - self._offsetCord.get_y())
+        if not self._negative:
+            self._baseCord.set_y(y - self._offsetCord.get_y())
+        else:
+            self._baseCord.set_y(y + self._offsetCord.get_y())
+
+    def adjust_x(self, dx):
+        self._baseCord.adjust_x(dx)
+    
+    def adjust_y(self, dy):
+        self._baseCord.adjust_y(dy)
     
     def __repr__(self):
-        return "RefCordinate(%s, %s, %s)" % (self._baseCord, self._offsetCord, self.Space)
+        return "RefCordinate(%s, %s, %s, %d)" % (self._baseCord, self._offsetCord, self.Space, self._negative)
 
     def __str__(self):
-        return "RefCordinate(%s, %s, %s)" % (self._baseCord, self._offsetCord, self.Space)
+        return "RefCordinate(%s, %s, %s, %d)" % (self._baseCord, self._offsetCord, self.Space, self._negative)
 
+class CordRectangle():
+    def __init__(self, bottom_left_cord, width, height):
+        self.width = width
+        self.height = height
+        
+        self.BottomLeft = bottom_left_cord
+        self.BottomRight = OffsetCordinate(bottom_left_cord, self.width, 0)
+        self.TopLeft = OffsetCordinate(bottom_left_cord, 0 , self.height)
+        self.TopRight = OffsetCordinate(bottom_left_cord, self.width , self.height)
+
+    def GetCorners(self):
+        return [self.BottomLeft, self.BottomRight, self.TopLeft, self.TopRight]
+
+
+    def __str__(self):
+        return ("CordRectangle((%d,%d), (%d,%d), (%d,%d), (%d,%d))" %
+                (self.BottomLeft.get_x(), self.BottomLeft.get_y(),
+                 self.BottomRight.get_x(), self.BottomRight.get_y(),
+                 self.TopLeft.get_x(), self.TopLeft.get_y(),
+                 self.TopRight.get_x(), self.TopRight.get_y()))
+   
+        
 class GameObject(object):
     '''
     Any element of the game
@@ -76,7 +168,13 @@ class GameObject(object):
     def Update(self, delta_t):
         pass
 
-    def Draw(self, xy_pos):
+    def Draw (self, xy_pos):
+        pass
+
+    def Draw_Platform(self, xy_pos):
+        pass
+
+    def Draw_Puzzle(self, xy_pos):
         pass
 
     def on_mouse_event(self, Event_Type, Chord, buttons, modifiers, gesture):
@@ -92,19 +190,56 @@ class GameObject(object):
         if self.parent_space is not None:
             return self.parent_space.GetCordinatesOfContainedObject(self)
         else:
-            return SpaceCordinate(None)
+            return SpaceCordinate(self)
 
+    def GetBoundingRectangle(self):
+        if self.parent_space is None:
+            return CordRectangle(SpaceCordinate(self), self.width, self.height)
+        
+        BaseCord = self.parent_space.GetCordinatesOfContainedObject(self)
+        return CordRectangle(BaseCord, self.width, self.height)
+
+    def CheckForIntersection(self, CordRect):
+        BottomLeft = CordRect.BottomLeft.AdaptCordinateToContainedObject(self)
+        BottomRight = CordRect.BottomRight.AdaptCordinateToContainedObject(self)
+        TopLeft = CordRect.TopLeft.AdaptCordinateToContainedObject(self)
+        TopRight = CordRect.TopRight.AdaptCordinateToContainedObject(self)
+
+        #test to see if one of the rectangle corners is inside of this objects bounds
+        cords = [BottomLeft, BottomRight, TopLeft, TopRight]
+        for cord in cords:
+            if (( 0 <= cord.get_x() < self.width ) and
+                ( 0 <= cord.get_y() < self.height)):
+                return True
+
+        #test to see if one of this objects corners is inside of the rectangle
+        cords = self.GetBoundingRectangle().GetCorners()
+        for cord in cords:
+            if (( BottomLeft.get_x() <= cord.get_x() < BottomRight.get_x() ) and
+                ( BottomLeft.get_y() <= cord.get_y() < TopLeft.get_y())):
+                return True
+
+        return False            
+            
     def TestCordIsWithinObject(self, Cord):
         if Cord is None:
             return False
 
         AdaptedCord = Cord.AdaptCordinateToContainedObject(self)
 
+        """print ("Testing (%d, %d) against (%d, %d)" %
+               ( AdaptedCord.get_x(), AdaptedCord.get_y(),
+                self.width, self.height))"""
         if (( 0 <= AdaptedCord.get_x() < self.width ) and
             ( 0 <= AdaptedCord.get_y() < self.height)):
             return True
         else:
             return False
+
+    def GetObjectsInRectangle(self, rect):
+        if not self.CheckForIntersection(rect):
+            return []
+        return [self]
 
 class CordinateSpace(GameObject):
     GameObjectType = "CordinateSpace"
@@ -122,6 +257,8 @@ class CordinateSpace(GameObject):
         del self.ContentObjects[TargetObject]
 
     def GetCordinatesOfContainedObject(self, TargetObject):
+        if TargetObject is self:
+            return SpaceCordinate(self)
         for GameObj in self.ContentObjects.keys():
             if TargetObject is GameObj:
                 return self.ContentObjects[TargetObject];
@@ -130,7 +267,20 @@ class CordinateSpace(GameObject):
                 OffsetCord = self.ContentObjects[SubSpace];
                 BaseCord = SubSpace.GetCordinatesOfContainedObject(TargetObject)
                 return RefCordinate(BaseCord, OffsetCord, self)
+        print( "Attempted to get cordinates of object %s from space %s, " +
+               "which does not house that object" %
+               (TargetObject, self))
         return None #This should blow up, letting us know when something has gone wrong            
+
+    def GetObjectsInRectangle(self, rect):
+        
+        if not self.CheckForIntersection(rect):
+            return []
+        objects = []
+        for GameObj in self.ContentObjects.keys():
+            if GameObj.CheckForIntersection(rect):
+                 objects.extend(GameObj.GetObjectsInRectangle(rect))
+        return objects
 
     def Update(self, delta_t):
         for GameObj in self.ContentObjects.keys():
