158cc50e8e9d75c611fa6ea23c2e107c3f87dd62
holomorph <holomorph@4f94c154-1fe4-7ef1-e36f-7715520f0143>
2011-04-06 07:08:44 +0000

    collision detection on all sides, and stop over about a half second when on the ground

--- trunk/pyweek12/ennead/platformer.py
+++ trunk/pyweek12/ennead/platformer.py
@@ -57,7 +57,7 @@ class Player(GameObject):
     def Update(self, dt):
         self.update_coordinates()
         self.apply_gravity(dt)
-        self.stop_falling_if_on_solid_tile()
+        self.apply_drag(dt)
 
         if keystates[key.RIGHT]:
             self.move_right(dt)
@@ -66,6 +66,10 @@ class Player(GameObject):
         if keystates[key.SPACE]:
             self.jump()
             
+        self.do_not_go_through_ceiling()
+        self.do_not_go_through_floor()
+        self.do_not_go_through_walls()
+
         self.cord.set_y(self.cord.get_y() + self.y_vel*dt)
         self.cord.set_x(self.cord.get_x() + self.x_vel*dt)
 
@@ -77,24 +81,73 @@ class Player(GameObject):
         if self.y_vel < -1*MAX_Y_VEL:
             self.y_vel = -1*MAX_Y_VEL;
 
+    def apply_drag(self, dt):
+        if self.is_colliding_below():
+            slowdown = dt*GROUND_DRAG
+            if slowdown > abs(self.x_vel):
+                self.x_vel=0
+            else:
+                self.x_vel -= self.x_vel/abs(self.x_vel) * slowdown
+
+    def do_not_go_through_floor(self):
+        if self.is_colliding_below() and self.y_vel < 0:
+            self.y_vel = 0
+    
+    def do_not_go_through_walls(self):
+        if self.is_colliding_on_right() and self.x_vel > 0:
+            self.x_vel = 0
+        elif self.is_colliding_on_left() and self.x_vel < 0:
+            self.x_vel = 0
+    
+    def do_not_go_through_ceiling(self):
+        if self.is_colliding_above() and self.y_vel > 0:
+            self.y_vel = 0
+
     def is_colliding_below(self):
-        x,y = self.cord.get_x(), self.cord.get_y()
-        lower_left_tile = find_tile_for_point(x+10,y)
-        lower_right_tile = find_tile_for_point(x-10+TILE_SIZE_IN_PIXELS,y)
-        if lower_left_tile == 0 and lower_right_tile == 0:
+        x_left = self.cord.get_x() + COLLISION_BOX_REDUCTION_IN_PIXELS
+        x_right = self.cord.get_x() + TILE_SIZE_IN_PIXELS - COLLISION_BOX_REDUCTION_IN_PIXELS 
+        y = self.cord.get_y()
+        lower_left_tile_type = find_tile_for_point(x_left, y)
+        lower_right_tile_type = find_tile_for_point(x_right, y)
+        if lower_left_tile_type == 0 and lower_right_tile_type == 0:
             return False
         return True
 
-    def stop_falling_if_on_solid_tile(self):
-        if self.is_colliding_below() and self.y_vel < 0:
-            self.y_vel = 0
+    def is_colliding_above(self):
+        x_left = self.cord.get_x() + COLLISION_BOX_REDUCTION_IN_PIXELS
+        x_right = self.cord.get_x() + TILE_SIZE_IN_PIXELS - COLLISION_BOX_REDUCTION_IN_PIXELS 
+        y = self.cord.get_y() + TILE_SIZE_IN_PIXELS
+        upper_left_tile_type = find_tile_for_point(x_left, y)
+        upper_right_tile_type = find_tile_for_point(x_right, y)
+        if upper_left_tile_type == 0 and upper_right_tile_type == 0:
+            return False
+        return True
 
+    def is_colliding_on_right(self):
+        x = self.cord.get_x() + TILE_SIZE_IN_PIXELS
+        y_bottom = self.cord.get_y() + COLLISION_BOX_REDUCTION_IN_PIXELS
+        y_top = self.cord.get_y() + TILE_SIZE_IN_PIXELS - COLLISION_BOX_REDUCTION_IN_PIXELS
+        lower_right_tile_type = find_tile_for_point(x, y_bottom)
+        upper_right_tile_type = find_tile_for_point(x, y_top)
+        if lower_right_tile_type == 0 and upper_right_tile_type == 0:
+            return False
+        return True
+
+    def is_colliding_on_left(self):
+        x = self.cord.get_x()
+        y_bottom = self.cord.get_y() + COLLISION_BOX_REDUCTION_IN_PIXELS
+        y_top = self.cord.get_y() + TILE_SIZE_IN_PIXELS - COLLISION_BOX_REDUCTION_IN_PIXELS
+        lower_left_tile_type = find_tile_for_point(x, y_bottom)
+        upper_left_tile_type = find_tile_for_point(x, y_top)
+        if lower_left_tile_type == 0 and upper_left_tile_type == 0:
+            return False
+        return True
 
     def move_right(self, dt):
-        self.cord.set_x(self.cord.get_x() + (20 * dt))
+        self.x_vel = MAX_X_VEL
 
     def move_left(self, dt):
-        self.cord.set_x(self.cord.get_x() - (20 * dt))
+        self.x_vel = -1*MAX_X_VEL
 
     def jump(self):
         if self.is_colliding_below():
